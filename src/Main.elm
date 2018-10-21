812f8488fc34bf1ff3df25e7a9674cb0cb350981
Ralf Northman <ralf.northman@bredband.net>
2018-10-21 09:39:21 +0200

    Beginning of tryout of RemoteData. Not compiling.

--- src/Main.elm
+++ src/Main.elm
@@ -3,18 +3,41 @@ module Main exposing (..)
 import Browser exposing (element)
 import Element exposing (..)
 import Element.Input as Input
+import Graphql.Http
+import RemoteData exposing (RemoteData)
+
+
+-- import Graphql.OptionalArgument exposing (OptionalArgument(..))
+-- graphql
+
+
+type alias Response =
+    List String
+
+
+type Msg
+    = GotResponse (RemoteData (Graphql.Http.Error Response) Response)
+
+query =
+  
+
+makeRequest =
+    query
+        |> Graphql.Http.queryRequest "http://localhost:4000/api"
+        |> Graphql.Http.send (RemoteData.fromResult >> GotResponse)
+
 
 
 -- model
 
 
 type alias Model =
-    Int
+    { searchWord : String, searchResult : String }
 
 
 init : () -> ( Model, Cmd msg )
 init _ =
-    ( 0, Cmd.none )
+    ( { searchWord = "tan", searchResult = "" }, Cmd.none )
 
 
 
